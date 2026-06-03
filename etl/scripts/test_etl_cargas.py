e973cde748ff69613b4c621b36365f67f9efda65
carlossobral <carlossobral87@gmail.com>
2026-06-02 22:54:29 -0300

    Update test_etl_cargas.py

--- etl/scripts/test_etl_cargas.py
+++ etl/scripts/test_etl_cargas.py
@@ -1,1 +1,25 @@
+from datetime import datetime
 
+from etl.database.supabase_client import supabase
+
+
+def main():
+    response = (
+        supabase.table("etl_cargas")
+        .insert(
+            {
+                "processo": "teste",
+                "inicio": datetime.utcnow().isoformat(),
+                "status": "SUCESSO",
+                "registros": 1,
+                "mensagem": "Primeiro teste do Sobral Invest"
+            }
+        )
+        .execute()
+    )
+
+    print(response.data)
+
+
+if __name__ == "__main__":
+    main()
