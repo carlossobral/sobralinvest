affd5bbc152c7b36fb609f8f248e9f578993ea7d
carlossobral <carlossobral87@gmail.com>
2026-07-03 19:47:46 -0300

    Update etl_fundamentos_cvm.py

--- etl/scripts/etl_fundamentos_cvm.py
+++ etl/scripts/etl_fundamentos_cvm.py
@@ -6,8 +6,8 @@ from zipfile import ZipFile
 from datetime import datetime, UTC
 from etl.database.supabase_client import supabase
 
-ANO_INICIAL = 2016
-ANO_FINAL = 2016
+ANO_INICIAL = 2017
+ANO_FINAL = 2017
 
 # Contas validadas
 MAPEAMENTO_DRE = {
